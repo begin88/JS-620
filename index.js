e2238416271f9db44638bb35eef54e0870cead14
begin88 <ihorbehen@gmail.com>
2021-08-04 20:48:06 +0300

    Home task

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 console.log("Behen");
 
 
-/*
+/* Tack 3
 let data = "dog";
 let note = "cat";
 data = note;
 console.log(note);
-/*
+*/
 
 /*
 let text;
@@ -16,11 +16,81 @@ text = "200";
 console.log(text);
 */
 
-/*
-const user = {
+
+
+
+/* Task 4
+let name = "Hello JS";
+console.log(typeof name);
+
+let data = 33;
+console.log(typeof data);
+
+let isName = true;
+isNane = false;
+console.log(typeof isName);
+
+let text;
+console.log(typeof text);
+
+let name = "Text";
+name = null;
+console.log(typeof name);
+*/
+
+
+/* Task 5
+let isAdult = confirm("Are you 18?");
+isAdult = 21;
+console.log(isAdult);
+*/
+
+
+/* Task 6
+let user = {
     firstName: "Igor",
     lastName: "Behen",
-    studyGroup: "620",
-    yeurOfBirth: "1988"
+    studyGroup: 620,
+    yeurOfBirth: 1988
 };
 
+let status = confirm("Are you Married?");
+console.log(status);
+status = Yes;
+console.log(status);
+*/
+
+/* Task 7
+let data = prompt("login");
+alert(data);
+let text = prompt("your email");
+alert(text);
+let pass = prompt("your password");
+alert(pass);
+
+*/
+
+/* Task 8
+let secondsInAMinute = 60;
+let minutesInAnHour = 60;
+let secondsInAnHour = secondsInAMinute * minutesInAnHour;
+alert(secondsInAnHour);
+
+*/
+/*
+let hoursInADay = 24;
+let secondsInAnHour = 3600;
+let secondsInADay = secondsInAnHour * hoursInADay;
+alert(secondsInADay);
+
+*/
+
+
+/*
+
+let dayInAMonth = 30;
+let secondsInADay = 86400;
+let secondsInAMonth = dayInAMonth * secondsInADay;
+alert(secondsInAMonth);
+
+*/
