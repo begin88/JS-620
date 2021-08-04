0b35dd7041dfb095d0f03b25dda282dface9c6dc
begin88 <ihorbehen@gmail.com>
2021-08-04 20:12:59 +0300

    Home tack1

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 //alert("Behen");
 console.log("Behen");
+
+
+/*
+let data = "dog";
+let note = "cat";
+data = note;
+console.log(note);
+/*
+
+/*
+let text;
+text = "100";
+text = "200";
+console.log(text);
+*/
+
+/*
+const user = {
+    firstName: "Igor",
+    lastName: "Behen",
+    studyGroup: "620",
+    yeurOfBirth: "1988"
+};
+
